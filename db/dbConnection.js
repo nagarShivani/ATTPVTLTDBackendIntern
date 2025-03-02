f3479ab613d40a1b010990d800f9e3627801e328
Disha Gupta <disha29082001@gmail.com>
2025-03-02 18:20:21 +0530

    GET Employees Data Task Done

--- db/dbConnection.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-
-
-mongoose.connect(`mongodb://${process.env.DATABASE_HOST}:${process.env.DATABASE_PORT}/${process.env.DATABASE_NAME}`).then(()=>{
-    console.log("Database connected successfully")
-})  
